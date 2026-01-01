03e17940d018c417e44980c59d88ea5973ee114e
Anurag Singh <96870454+AnuragSingh2511@users.noreply.github.com>
2026-01-01 12:54:46 +0530

    feat: types in Typescript

---
+++ src/typesinTS.ts
@@ -0,0 +1,15 @@
+let drink = "chai"
+// drink = 0
+
+let cups = Math.random() > 0.5 ? 10 : 5; 
+
+//ternary operator - if it is greater than 0.5 then add 10 otherwise add 5
+// console.log(cups); // node src then press tab and select typesinTS and tab
+
+let channelName = "AnuFlows"
+// channelName = 12221 //-> this wont work
+
+let chaiFlavour:string = "masala Chai"
+chaiFlavour = "Ginger Chai" //masala Chai overwritten to Ginger Chai...this is nice
+
+let chaiOrder: boolean
